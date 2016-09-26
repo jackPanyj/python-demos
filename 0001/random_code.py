2f64f28971f03e1807e04e5585a1f72a1d731114
panshao <panshao@panshaodeMacBook-Pro.local>
2016-09-26 15:41:14 +0800

    done one day

---
+++ 0001/random_code.py
@@ -0,0 +1,14 @@
+import uuid
+
+'''第 0001 题：做为 Apple Store App 独立开发者，你要搞限时促销，为你的应用生成激活码（或者优惠券），使用 Python 如何生成 200 个激活码（或者优惠券）？'''
+def random_code():
+	f = open('./code.txt', 'w')
+
+	for i in range(200):
+		s = str(uuid.uuid4())[0:8]
+		f.write(s + '\n')
+	f.close()
+
+
+if __name__ == '__main__':
+	random_code()
