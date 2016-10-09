4844c2bfd4546de810c175afb394bf30cbee8015
jackPanyj <1016432619@qq.com>
2016-10-09 18:40:31 +0800

    add comments

--- 0004/count_word.py
+++ 0004/count_word.py
@@ -1,12 +1,21 @@
+#!/usr/bin/env python
+# -*-coding:utf-8-*-
+
+
+# 第 0004 题：任一个英文的纯文本文件，统计其中的单词出现的个数
 import os
 import re
+
+# 利用str.split()来分割单词
 def method1(file_path):
     with open(file_path, 'r') as f:
         print len(f.read().split())
 
+# 利用bash的命令` wc -w english.txt `来统计单词数量
 def method2(file_path):
     print os.popen('wc -w {0}'.format(file_path)).read()
 
+# 利用正则表达式来统计数量
 def method3(file_path):
     with open(file_path, 'r') as f:
         words = re.findall(r'[a-zA-Z0-9]+', f.read())
